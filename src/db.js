e06041023d6e13326f7d9fb4b68eb85f150b9acb
BastianF <bastian.fuentes.gaete@gmail.com>
2024-06-30 17:29:33 -0400

    Se agrego port al bd

--- src/db.js
+++ src/db.js
@@ -2,6 +2,7 @@ import { Sequelize } from "sequelize";
 import { DB_HOST, DB_DATABASE, DB_PASSWORD, DB_USER, DB_PORT} from "./config.js";
 const sequelize = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
+    port: DB_PORT,
     dialect:'mysql'
 });
 
